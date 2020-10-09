e67c48de738c4dde7fdbbd7aa4787cace05004c2
Pedro Jorquera <pjorquera@okode.com>
2020-10-09 19:00:21 +0200

    Added GreetingController

---
+++ backend/src/main/java/com/okode/backend/controller/GreetingController.java
@@ -0,0 +1,18 @@
+package com.okode.backend.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+public class GreetingController {
+
+    @GetMapping("/hello/{name}")
+    public Object hello(@PathVariable String name) {
+        return new Object() { public final String response = "Hello, " + name; };
+    }
+
+}

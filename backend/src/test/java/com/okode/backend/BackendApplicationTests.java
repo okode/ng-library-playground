99418e09eb779681a55d9a849c846ef050b31583
Pedro Jorquera <pjorquera@okode.com>
2020-10-10 18:35:00 +0200

    Added tests

--- backend/src/test/java/com/okode/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/okode/backend/BackendApplicationTests.java
@@ -1,13 +1,29 @@
 package com.okode.backend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.UUID;
 
 @SpringBootTest
+@AutoConfigureWebTestClient
 class BackendApplicationTests {
 
+	@Autowired
+	private WebTestClient webTestClient;
+
 	@Test
-	void contextLoads() {
+	public void greetingControllerTest() {
+		final var name = UUID.randomUUID().toString();
+		webTestClient.get().uri("/hello/" + name)
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody().jsonPath("$.response").isEqualTo("Hello, " + name);
 	}
 
 }

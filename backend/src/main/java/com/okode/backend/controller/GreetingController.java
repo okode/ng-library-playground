ed61b9c3e6e8c18413cf88757ff6f2883c29d795
Pedro Jorquera <pjorquera@okode.com>
2020-10-10 13:11:12 +0200

    Removed unneeded annotations

--- backend/src/main/java/com/okode/backend/controller/GreetingController.java
+++ backend/src/main/java/com/okode/backend/controller/GreetingController.java
@@ -1,13 +1,10 @@
 package com.okode.backend.controller;
 
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class GreetingController {
 
     @GetMapping("/hello/{name}")

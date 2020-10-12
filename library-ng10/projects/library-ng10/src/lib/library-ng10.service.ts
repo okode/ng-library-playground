c75dc08b19d90d39602bb3a456886d66cd70036a
Pedro Jorquera <pjorquera@okode.com>
2020-10-12 10:05:43 +0200

    Fixed lint

--- library-ng10/projects/library-ng10/src/lib/library-ng10.service.ts
+++ library-ng10/projects/library-ng10/src/lib/library-ng10.service.ts
@@ -9,7 +9,7 @@ export class LibraryNg10Service {
   constructor(private http: HttpClient) { }
 
   public greeting(name: string) {
-    return this.http.get< { response: string } >(`http://localhost:8080/greeting/${name}`);
+    return this.http.get<{ response: string }>(`http://localhost:8080/greeting/${name}`);
   }
 
 }
